73dc7cffe0569bfce57757d9e560c98fbf2aa9fc
achinthajayashan <achintha.uns.2018@gmail.com>
2024-03-07 08:49:08 +0530

    footer started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from "./header/navbar/navbar.component";
 import {HeaderComponent} from "./header/header.component";
 import {HomeComponent} from "./home/home.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
