a6e31da34af8955fc6bdd9d617f90a587ced360a
achinthajayashan <achintha.uns.2018@gmail.com>
2024-03-11 20:41:59 +0530

    products page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      products works!
-    </p>
-  `,
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
+  items = [
+    { id: 1, name: 'Fresh Pear' , weight:'1KG' ,price:'Rs.120.00' , image:'Pear_4.png'},
+    { id: 2, name: 'Sweet Watermelon' , weight:'1KG' ,price:'Rs.120.00' ,image:'Watermelon_4.png' },
+    { id: 3, name: 'Pumpkin' , weight:'1KG' ,price:'Rs.120.00' ,image:'Pumpkin_3.png' },
+    { id: 3, name: 'Strawberry' , weight:'1KG' ,price:'Rs.120.00' ,image:'Strawberry_1.png' },
+    { id: 1, name: 'Fresh Pear' , weight:'1KG' ,price:'Rs.120.00' , image:'Pear_4.png'},
+    { id: 2, name: 'Sweet Watermelon' , weight:'1KG' ,price:'Rs.120.00' ,image:'Watermelon_4.png' },
+    { id: 3, name: 'Pumpkin' , weight:'1KG' ,price:'Rs.120.00' ,image:'Pumpkin_3.png' },
+    { id: 3, name: 'Strawberry' , weight:'1KG' ,price:'Rs.120.00' ,image:'Strawberry_1.png' },
+  ];
 }
