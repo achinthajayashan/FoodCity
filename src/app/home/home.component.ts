cd400380906d1596c69bc3426b14dacb05813d9f
achinthajayashan <achintha.uns.2018@gmail.com>
2024-03-11 19:31:01 +0530

    products loaded to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,7 +21,9 @@ export class HomeComponent {
   ]
 
   items = [
-    { id: 1, name: 'Fresh Pear' , weight:'1KG' ,price:'120LKR'},
-    { id: 2, name: 'Sweet Watermelon' , weight:'1KG' ,price:'120LKR' },
+    { id: 1, name: 'Fresh Pear' , weight:'1KG' ,price:'Rs.120.00' , image:'Pear_4.png'},
+    { id: 2, name: 'Sweet Watermelon' , weight:'1KG' ,price:'Rs.120.00' ,image:'Watermelon_4.png' },
+    { id: 3, name: 'Pumpkin' , weight:'1KG' ,price:'Rs.120.00' ,image:'Pumpkin_3.png' },
+    { id: 3, name: 'Strawberry' , weight:'1KG' ,price:'Rs.120.00' ,image:'Strawberry_1.png' },
   ];
 }
