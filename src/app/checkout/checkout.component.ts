7ed863a9ab84d952348e65b26815370b5b822828
achinthajayashan <achintha.uns.2018@gmail.com>
2024-03-12 22:22:59 +0530

    checkout form

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,16 +1,50 @@
 import { Component } from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      checkout works!
-    </p>
-  `,
+  imports: [
+    NgIf,
+    ReactiveFormsModule
+  ],
+  templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+
+  checkoutForm = this.fb.group({
+    fullName: ['', Validators.required,],
+    address: ['', Validators.required],
+    city: ['', Validators.required],
+    district: ['', Validators.required],
+    email: ['', [Validators.required , Validators.email]],
+    contact: [1, Validators.required],
+  });
+
+  isSubmitted = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  // ngOnInit(): void {
+  //   this.registerForm.get('roleId')?.valueChanges.subscribe((roleId) => {
+  //     console.log('SEND API REQUEST AND UPDATE ROLE', roleId);
+  //   });
+  // }
+
+  onSubmit(): void {
+    console.log(
+      'submitted form',
+      this.checkoutForm.value,
+      this.checkoutForm.invalid
+    );
+
+    alert(
+      this.checkoutForm.value.email
+    )
+
+    this.isSubmitted = true;
+  }
 
 }
